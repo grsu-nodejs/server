b86f7153a8c6057cfc40d9d3a167b6001d5f17f7
gleifvo <77hagor777@gmail.com>
2016-04-06 23:20:06 +0300

    start to do writing articles headers in database

--- src/server/responseHandler.js
+++ src/server/responseHandler.js
@@ -9,69 +9,106 @@ var assert = require('assert');
 var table = 'articles';
 var url = 'mongodb://localhost:27017/s13';
 
+
+var monthsNames = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
 function jsonResponse(res, content) {
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+	res.writeHead(200, {
+		'Content-Type': 'application/json; charset=utf-8'
+	});
 
-    res.write(JSON.stringify(content));
+	res.write(JSON.stringify(content));
 
-    res.end('');
+	res.end('');
 }
 
 function saveAndReturnArticle(res, content) {
-    jsonResponse(res, content);
+	jsonResponse(res, content);
 
-    mongoConnectWithMethod(insertArticles, content);
+	mongoConnectWithMethod(insertArticles, content);
 }
 
 function saveAndReturnParagraphs(res, content, articleId) {
-    jsonResponse(res, content);
+	jsonResponse(res, content);
 
-    mongoConnectWithMethod(insertParagraphs, content, articleId);
+	mongoConnectWithMethod(insertParagraphs, content, articleId);
 }
 
 function mongoConnectWithMethod(dbMethod, content, id, callback) {
-    MongoClient.connect(url, function (err, db) {
-        dbMethod(db, content, function (content) {
-            db.close();
-            if (callback) {
-                callback(content);
-            }
-        }, id);
-    });
+	MongoClient.connect(url, function (err, db) {
+		dbMethod(db, content, function (content) {
+			db.close();
+			if (callback) {
+				callback(content);
+			}
+		}, id);
+	});
 }
 
 function insertArticles(db, articles, callback) {
-    db.collection(table).insertMany(articles);
-    callback();
+	db.collection(table).insertMany(articles);
+	callback();
 }
 
 function insertParagraphs(db, paragraphs, callback, articleId) {
-    db.collection(table).updateOne({_id: articleId}, {$set: {paragraphs: paragraphs}});
-    callback();
+	db.collection(table).updateOne({
+		_id: articleId
+	}, {
+		$set: {
+			paragraphs: paragraphs
+		}
+	});
+	callback();
 }
 
 function returnArticles(response, year, month, day) {
-    //TODO articles by date
-    scraper.scrapWithParseMethod(response, saveAndReturnArticle, parser.parseForEntries, 'date', year, month, day);
+	var date = day + ' ' + monthsNames[month - 1] + ' ' + year + ' года';
+
+	var content;
+	mongoConnectWithMethod(findArticles, content, date, function (content) {
+		if (content) {
+			console.log("fromBase");
+			jsonResponse(response, content);
+		} else {
+			console.log("no in base");
+			scraper.scrapWithParseMethod(response, saveAndReturnArticle, parser.parseForEntries, 'date', year, month, day);
+		}
+	});
+
 }
 
 function findParagraphs(db, paragraphs, callback, articleId) {
-    db.collection('articles').find({_id: articleId}, {paragraphs: 1}).limit(1).next(function (err, doc) {
-        paragraphs = doc.paragraphs;
-        callback(paragraphs);
-    });
+	db.collection('articles').find({
+		_id: articleId
+	}, {
+		paragraphs: 1
+	}).limit(1).next(function (err, doc) {
+		paragraphs = doc.paragraphs;
+		callback(paragraphs);
+	});
+}
+
+function findArticles(db, articles, callback, date) {
+	db.collection('articles').find({
+		date: date
+	}).forEach(function (doc) {
+		console.log(doc);
+		articles = doc;
+	});
+	
+
 }
 
 function returnParagraphs(response, id, content) {
-    mongoConnectWithMethod(findParagraphs, content, id, function (content) {
-        if (content) {
-            console.log("fromBase");
-            jsonResponse(response, content);
-        } else {
-            console.log("no in base");
-            scraper.scrapWithParseMethod(response, saveAndReturnParagraphs, parser.parseForParagraphs, id);
-        }
-    });
+	mongoConnectWithMethod(findParagraphs, content, id, function (content) {
+		if (content) {
+			console.log("fromBase");
+			jsonResponse(response, content);
+		} else {
+			console.log("no in base");
+			scraper.scrapWithParseMethod(response, saveAndReturnParagraphs, parser.parseForParagraphs, id);
+		}
+	});
 }
 
 exports.returnParagraphs = returnParagraphs;
