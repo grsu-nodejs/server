2c2e62c693cfc480bb6001c4f75c31b4bec23a6a
gleifvo <77hagor777@gmail.com>
2016-09-04 19:03:35 +0300

    Add ul with articles of hardcoded day

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
 import '../assets/css/style.css';
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: []
+        };
+        this.getData();
+    }
 
-class App extends React.Component {
     render() {
         return (
-            <p> Hello {this.props.author}!</p>
+            <ul>
+                {this.state.articles.map((article) => {
+                    return (
+                        <li>
+                            <h2>{article.title}</h2>
+                            <p>{article.auhtor} {article.date} {article.time}</p>
+                            <p>{article.text}</p>
+                        </li>
+                    );
+                })}
+            </ul>
         );
+    }
+
+    getData() {
+        console.log("exec");
+        fetch('/day?year=2016&month=07&day=15')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({articles: data});
+            })
     }
 }
 
 render(
-    <App author="Name"/>,
+    <App/>,
     document.getElementById('app')
 );
+
