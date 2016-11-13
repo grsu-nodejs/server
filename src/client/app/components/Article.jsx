c7e1b0d64ea3fcc562f4706b0301ebb04960dc1e
gleifvo <77hagor777@gmail.com>
2016-11-13 15:27:48 +0300

    Add datepicker Implement behaviour for loading articles for day

---
+++ src/client/app/components/Article.jsx
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+export default class Article extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isCollapsed: true,
+            paragraphs: []
+        };
+
+        this.handleArticleParagraphsView = this.handleArticleParagraphsView.bind(this);
+    }
+
+    static createQueryStringForArticle(id) {
+        return `/article?id=${id}`;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.article._id != nextProps.article._id) {
+            this.state = {
+                isCollapsed: true,
+                paragraphs: []
+            };
+        }
+    }
+
+    handleArticleParagraphsView() {
+        if (this.state.paragraphs.length) {
+            this.setState({
+                isCollapsed: !this.state.isCollapsed
+            });
+            return;
+        }
+        this.fetchArticleParagraphs();
+    }
+
+    fetchArticleParagraphs() {
+        fetch(Article.createQueryStringForArticle(this.props.article._id)).then((response) => {
+            return response.json();
+        }).then((paragraphs) => {
+            this.setState({
+                isCollapsed: !this.state.isCollapsed,
+                paragraphs: paragraphs
+            });
+        })
+    }
+
+    render() {
+        let articleBody = this.state.isCollapsed ? (
+            <p>{this.props.article.text}</p>
+        ) : (this.state.paragraphs.map((paragraph, index) => {
+            return (
+                <div key={index}>
+                    {paragraph.imgsrc ?
+                        <img src={paragraph.imgsrc}/>
+                        :
+                        <p>{paragraph.text}</p>
+                    }
+                </div>
+            );
+        }));
+
+        return (
+            <div>
+                <h2 onClick={this.handleArticleParagraphsView}>{this.props.article.title}</h2>
+                <p>{this.props.article.author} {this.props.article.date} {this.props.article.time}</p>
+                {articleBody}
+                <button onClick={this.handleArticleParagraphsView}>
+                    {this.state.isCollapsed ? "Развернуть" : "Свернуть" }
+                </button>
+            </div>
+        );
+    }
+}
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,39 +1,60 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import Article from './components/article';
+import Article from './components/Article';
+
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
 import '../assets/css/style.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            articles: []
+            articles: [],
+            date: moment(),
         };
-        this.fetchData();
+        this.fetchArticles(this.state.date);
+        this.handleDateChange = this.handleDateChange.bind(this);
+    }
+
+    static createQueryStringForDay(date) {
+        return date.format('[/day?year=]YYYY[&month=]MM[&day=]DD');
+    }
+
+    fetchArticles(date) {
+        fetch(App.createQueryStringForDay(date)).then((response) => {
+            return response.json();
+        }).then((data) => {
+            this.setState({
+                articles: data
+            });
+        });
+    }
+
+    handleDateChange(date) {
+        this.setState({
+            date: date
+        });
+        this.fetchArticles(date);
     }
 
     render() {
         return (
-            <ul>
-                {this.state.articles.map((article, index) => {
-                    return (
-                        <li key={index}>
-                            <Article article={article}/>
-                        </li>
-                    );
-                })}
-            </ul>
+            <div>
+                <DatePicker selected={this.state.date} onChange={this.handleDateChange}/>
+                <ul>
+                    {this.state.articles.map((article, index) => {
+                        return (
+                            <li key={index}>
+                                <Article article={article}/>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
         );
-    }
-
-    fetchData() {
-        fetch('/day?year=2016&month=07&day=15')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                this.setState({articles: data});
-            })
     }
 }
 
@@ -41,4 +62,3 @@ render(
     <App/>,
     document.getElementById('app')
 );
-
