6985ad4a49979565e6e4e0406c3ee4f97bcd44ae
Vadzim Hleif <77hagor777@gmail.com>
2017-02-16 10:52:00 +0300

    Add loading all styles in separate file, update webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const webpack = require('webpack');
 const path = require('path');
-
 const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 const APP_DIR = path.resolve(__dirname, 'src/client/app');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const config = {
-    entry: APP_DIR + '/index',
+module.exports = {
+    entry: APP_DIR,
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
     module: {
         loaders: [
-            {test: /\.jsx?$/, include: APP_DIR, loader: 'babel', query: {presets: ['react']}},
-            {test: /\.css$/, loader: "style-loader!css-loader"},
-            {test: /\.less$/, loader: "style!css!autoprefixer!less"}
-        ]
+            {
+                test: /\.jsx?$/, include: APP_DIR, loader: 'babel-loader', query: {presets: ['react']}
+            },
+            {
+                test: /\.css$/, loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader!less-loader"})
+            }
+        ],
     },
+    plugins: [
+        new ExtractTextPlugin({filename: 'bundle.css'})
+    ],
     watch: true,
     devtool: "source-map",
     resolve: {
-        extensions: ['.jsx', '.js', '']
+        extensions: ['.jsx', '.js']
     }
 };
-
-module.exports = config;
